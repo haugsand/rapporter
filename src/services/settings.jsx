67091fec800fe1258704dac594c5fb07f3035680
Magnus Haugsand <haugsand@gmail.com>
2017-09-12 22:13:40 +0200

    Refactoring

---
+++ src/services/settings.jsx
@@ -0,0 +1,47 @@
+export default function getSettings (routeParams) {
+
+	console.log(routeParams);
+
+	let settings = {};
+
+	settings = {
+		vegobjekttype: routeParams.vegobjekttype,
+		result: routeParams.result,
+
+		row: routeParams.row.split('-')[0].split(':')[0],
+		hasSubrow: false,
+		subrow: '',
+		hasRowfilter: false,
+		rowfilter: '',
+
+		column: routeParams.column.split(';')[0],
+		hasColumnEgenskapstype: false,
+		columnEgenskapstype: '',
+		hasColumnInterval: false,
+		columnInterval: '' 
+
+	}
+
+
+	if (routeParams.row.split('-').length > 1) {
+		settings.hasSubrow = true;
+		settings.subrow = routeParams.row.split('-')[1];
+	}
+
+    if (routeParams.row.split(':').length > 1) {
+    	settings.hasRowfilter = true;
+        settings.rowfilter = routeParams.row.split('-')[0].split(':')[1];
+    }
+
+    if (routeParams.column.split(':').length > 1) {
+    	settings.hasColumnEgenskapstype = true;
+        settings.columnEgenskapstype = parseInt(routeParams.column.split(':')[1].split(';')[0], 10);
+    }
+
+	if (routeParams.column.split(';').length > 1) {
+		settings.hasColumnInterval = true;
+		settings.columnInterval = routeParams.column.split(';')[1].split(',');
+	}
+
+	return settings;
+}
